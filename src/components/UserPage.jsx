c4bf97e70fafac58321414ed23a5854ca9952ff6
IngDavidTM <david-1011@hotmail.com>
2023-05-12 16:26:43 -0500

    Style header

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -15,9 +15,11 @@ const UserPage = () => {
           <a className="textYellow weightLight" href="https://torre.co/?r=eJTS6tfg" target="_blank" rel="noreferrer">SIGN IN</a>
         </div>
       </nav>
-      <section>
-        <header>
-          <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="profile" />
+      <section className="infoSection flexColumn">
+        <header className="userInfo flexColumn">
+          <div className="hexagon flex">
+            <img className="userImage" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="profile" />
+          </div>
           <h2>Camila Helers</h2>
         </header>
         <div>
