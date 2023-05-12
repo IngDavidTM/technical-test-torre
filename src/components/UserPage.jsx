5b0df58f8722b2fafe7d25da0d83f7fea7a29b1e
IngDavidTM <david-1011@hotmail.com>
2023-05-12 17:20:15 -0500

    Add stye to skills

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -7,11 +7,11 @@ const UserPage = () => {
     <>
       <nav className="navBar flex">
         <div className="flex">
-          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon className="cursor" icon={faBars} />
           <h1>torre<span className='textYellow weightNormal'>.co</span></h1>
         </div>
         <div className="flex">
-          <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon className="cursor" icon={faSearch} />
           <a className="textYellow weightLight" href="https://torre.co/?r=eJTS6tfg" target="_blank" rel="noreferrer">SIGN IN</a>
         </div>
       </nav>
@@ -20,41 +20,41 @@ const UserPage = () => {
           <div className="hexagon flex">
             <img className="userImage" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="profile" />
           </div>
-          <h2>Camila Helers</h2>
+          <h2 className='weightBold'>Camila Helers</h2>
         </header>
-        <div>
-          <h3>Skills and interests:</h3>
-          <ul>
-            <li><FontAwesomeIcon icon={faPersonBiking} /> Master
-              <ul>
+        <div className="skills flexColumn">
+          <h3 className='weightNormal'>Skills and interests:</h3>
+          <ul className="skillsLevel flexColumn">
+            <li className="level weightBold"><FontAwesomeIcon className="icons" icon={faPersonBiking} /> ㅤMaster
+              <ul className="technologies flex">
                 <li><button>one</button></li>
                 <li><button>two</button></li>
                 <li><button>three</button></li>
               </ul>
             </li>
-            <li><FontAwesomeIcon icon={faPersonRunning} /> Expert
-              <ul>
+            <li className="level weightBold"><FontAwesomeIcon className="icons" icon={faPersonRunning} /> ㅤExpert
+              <ul className="technologies flex">
                 <li><button>one</button></li>
                 <li><button>two</button></li>
                 <li><button>three</button></li>
               </ul>
             </li>
-            <li><FontAwesomeIcon icon={faPersonRunning} /> Proficient
-              <ul>
+            <li className="level weightBold"><FontAwesomeIcon className="icons" icon={faPersonRunning} /> ㅤProficient
+              <ul className="technologies flex">
                 <li><button>one</button></li>
                 <li><button>two</button></li>
                 <li><button>three</button></li>
               </ul>
             </li>
-            <li><FontAwesomeIcon icon={faPersonWalking} /> Beginner
-              <ul>
+            <li className="level weightBold"><FontAwesomeIcon className="icons" icon={faPersonWalking} /> ㅤBeginner
+              <ul className="technologies flex">
                 <li><button>one</button></li>
                 <li><button>two</button></li>
                 <li><button>three</button></li>
               </ul>
             </li>
-            <li><FontAwesomeIcon icon={faChild} /> No experience, but interested
-              <ul>
+            <li className="level weightBold"><FontAwesomeIcon className="icons" icon={faChild} /> ㅤNo experience, but interested
+              <ul className="technologies flex">
                 <li><button>one</button></li>
                 <li><button>two</button></li>
                 <li><button>three</button></li>
