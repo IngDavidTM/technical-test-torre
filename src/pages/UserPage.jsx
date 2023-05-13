39f81febf17c29288e5bcc45482187f4532d4ea9
IngDavidTM <david-1011@hotmail.com>
2023-05-13 07:42:59 -0500

    Add rule

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -11,7 +11,6 @@ const UserPage = () => {
   const { data, fetchData } = useContext(AppContext);
   useEffect(() => {
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const [show, setShow] = useState('main');
   const [showButton, setShowButton] = useState(true);
