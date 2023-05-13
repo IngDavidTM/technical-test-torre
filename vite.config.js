32833b944e57ed8e381ef275a9807f5c3da8ff02
IngDavidTM <david-1011@hotmail.com>
2023-05-13 11:20:38 -0500

    Change vite

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/technical-test-torre/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bio.torre.co',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
